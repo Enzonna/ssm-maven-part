3df8fee63bb240fd9c016f5dbc9d0640641c1709
Enzonna <1825437118@qq.com>
2024-04-20 15:48:13 +0800

    Initial commit

---
+++ maven-micro-shop/user-service/src/main/java/com/enzo/Main.java
@@ -0,0 +1,16 @@
+package com.enzo;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.enzo
+ * Description:
+ *
+ * @Author ENZO
+ * @Create ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
